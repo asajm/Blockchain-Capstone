6ed92e110c4f482baa3489ad60bed94395ba5aed
Ahmed Almalki <mr.asajm@gmail.com>
2019-05-07 23:14:50 +0300

    Deployment + Market Place

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,12 +1,21 @@
 // migrating the appropriate contracts
 // var SquareVerifier = artifacts.require("./SquareVerifier");
-// var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier");
-var AhmedToken = artifacts.require("AhmedToken");
+var SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
+// var AhmedToken = artifacts.require("AhmedToken");
 var Verifier = artifacts.require("Verifier");
 
-module.exports = function(deployer) {
+module.exports = async function (deployer) {
   // deployer.deploy(SquareVerifier);
   // deployer.deploy(SolnSquareVerifier);
-  deployer.deploy(AhmedToken);
-  deployer.deploy(Verifier);
+  // deployer.deploy(AhmedToken);
+  await deployer.deploy(Verifier)
+  await deployer.deploy(SolnSquareVerifier, Verifier.address)
+
+  console.log('# Verifier address: %s', Verifier.address)
+  console.log('# Verifier transaction: %s', Verifier.transactionHash)
+  // console.log(Verifier.abi)
+
+  console.log('# SolnSquareVerifier address: %s', SolnSquareVerifier.address)
+  console.log('# SolnSquareVerifier transaction: %s', SolnSquareVerifier.transactionHash)
+  // console.log(SolnSquareVerifier.abi)
 };
