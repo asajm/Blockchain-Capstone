39509dab14efaad572c240a3de857b8ed18580eb
Ahmed Almalki <mr.asajm@gmail.com>
2019-05-07 20:13:45 +0300

    writes and passes "SolnSquareVerifier.sol" & "TestSolnSquareVerifier.js"

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,59 +1,75 @@
 pragma solidity ^0.5.0;
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+import './ERC721Mintable.sol';
+import './verifier.sol';
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
+contract MyVerifier is Verifier {
 
+}
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is AhmedToken {
+
+    using SafeMath for uint256;
+
+    // TODO define a solutions struct that can hold an index & an address
+    struct Solution {
+        uint256 _index;
+        address _address;
+    }
+
+    // TODO define an array of the above struct
+    // Solution[] solutions;
+    uint256 solutionCount = 0;
+
+    // TODO define a mapping to store unique solutions submitted
+    mapping (bytes32 => Solution) uniqueSolutions;
+
+    MyVerifier myVerifier;
+
+    // TODO Create an event to emit when a solution is added
+    event SolutionAdded(uint256 _index, address _address, bytes32 _key);
 
 
+    constructor (address contractVerifierAddress) public {
+        myVerifier = MyVerifier(contractVerifierAddress);
+    }
 
-// TODO define a solutions struct that can hold an index & an address
+    // TODO Create a function to add the solutions to the array and emit the event
+    function addSolution(uint256 _index, address _address) public {
+        bytes32 _key = keccak256(abi.encode(_index, _address));
+        require(uniqueSolutions[_key]._address == address(0), 'the solution has been added');
+        uniqueSolutions[_key] = Solution(_index, _address);
+        solutionCount = solutionCount.add(1);
+        emit SolutionAdded(_index, _address, _key);
+    }
 
+    function getSolutionCount() public returns(uint256){
+        return solutionCount;
+    }
 
-// TODO define an array of the above struct
-
-
-// TODO define a mapping to store unique solutions submitted
-
-
-
-// TODO Create an event to emit when a solution is added
-
-
-
-// TODO Create a function to add the solutions to the array and emit the event
-
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // TODO Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSuplly
+    function mintNFT(
+            address to,
+            uint256 tokenId,
+            uint[2] memory a,
+            uint[2] memory a_p,
+            uint[2][2] memory b,
+            uint[2] memory b_p,
+            uint[2] memory c,
+            uint[2] memory c_p,
+            uint[2] memory h,
+            uint[2] memory k,
+            uint[2] memory input
+        ) public returns(bool) {
+            require(myVerifier.verifyTx(a, a_p, b, b_p, c, c_p, h, k, input) == true , 'the verification is not correct');
+            addSolution(tokenId, to);
+            return super.mint(to, tokenId);
+    }
+}
 
 
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,67 @@
-// Test if a new solution can be added for contract - SolnSquareVerifier
+let Verifier = artifacts.require('Verifier');
+let SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+let proof = require('../../zokrates/code/square/proof').proof
+let input = require('../../zokrates/code/square/proof').input
+var Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545/'));
 
-// Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+
+contract('SolnSquareVerifier', accounts => {
+    const owner = accounts[0];
+    const account_one = accounts[1];
+
+    describe("The test cases in 'SolnSquareVerifier.js'", function () {
+        beforeEach(async function () {
+            let verifier = await Verifier.new({ from: owner });
+            this.contract = await SolnSquareVerifier.new(verifier.address, { from: owner });
+        })
+
+        // Test if a new solution can be added for contract - SolnSquareVerifier
+        it('Test if a new solution can be added for contract - SolnSquareVerifier', async function () {
+
+            let oldCount = await this.contract.getSolutionCount.call()
+            // console.log(oldCount)
+
+            try {
+                let result = await this.contract.addSolution(1, account_one)
+                // console.log(result.logs)
+            } catch (error) {
+                // console.log(error)
+            }
+
+            let newCount = await this.contract.getSolutionCount.call()
+            // console.log(newCount)
+
+            oldCount = parseInt(oldCount)
+            newCount = parseInt(newCount)
+
+            // console.log('old count = %s\nnew count = %s', parseInt(oldCount), parseInt(newCount))
+            assert.equal(newCount, oldCount + 1, 'the solutions has to be increased 1')
+
+        })
+
+        // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+        it('Test if an ERC721 token can be minted for contract - SolnSquareVerifier', async function () {
+            let result = false
+            try {
+                result = await this.contract.mintNFT.call(
+                    account_one,
+                    2,
+                    proof.A,
+                    proof.A_p,
+                    proof.B,
+                    proof.B_p,
+                    proof.C,
+                    proof.C_p,
+                    proof.H,
+                    proof.K,
+                    input,
+                    { from: owner })
+                // console.log(result)
+            } catch (error) {
+                console.log(error)
+            }
+            assert.equal(result, true, 'the solutions has to be increased 1')
+        })
+    });
+})
